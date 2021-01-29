38ff265bb493d4520983e105ad091252ce539c0d
jagtheripper <jguapin21@gmail.com>
2021-01-29 08:38:43 +0800

    added migration for item history

---
+++ database/migrations/2021_01_29_003358_create_item_histories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemHistoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item_histories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('item_id');
+            $table->integer('location_id');
+            $table->integer('user_id');
+            $table->text('description');
+            $table->integer('status_id');
+            $table->timestamp('updated_at')->nullable();
+            $table->timestamp('created_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item_histories');
+    }
+}
